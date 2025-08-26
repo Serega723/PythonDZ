769303a39111927788579b29dc072ce21cf24939
Serega723 <sergkendez@yandex.ru>
2025-08-26 20:12:52 +0300

    PYdz1

---
+++ Desktop/PYTHON/index.py
@@ -0,0 +1,49 @@
+""" a = int(input("Enter the number: "))
+b = int(input("Enter the number: "))
+c = int(input("Enter the number: "))
+print(a+b+c)
+print(a*b*c) """
+
+""" zarplata = int(input("Enter the number: "))
+sumpl = int(input("Enter the number: "))
+dolg = int(input("Enter the number: "))
+print(zarplata-sumpl-dolg) """
+
+""" d2 = int(input("Enter the number: "))
+d1 = int(input("Enter the number: "))
+print(d1*d2/2) """
+
+""" print("to be")
+print("or not")
+print("to be") """
+""" 
+print("Life is what happens")
+print("   when")
+print("        you're busy making other plans.")
+print("                                            -John Lennon") """
+
+""" a = int(input("Enter the number: "))
+b = int(input("Enter the number: "))
+c = int(input("Enter the number: "))
+print(str(a)+str(b)+str(c)) """
+
+""" number = int(input("Enter the number: "))
+num1 = number % 10
+num2 = number % 100//10
+num3 = number % 1000//100
+num4 = number // 1000
+print(num1*num2*num3*num4) """
+
+
+""" metrs = int(input("Enter the number: "))
+santimers = print(metrs*100)
+decimeters = print(metrs*10)
+millimeters = print(metrs*1000)
+mils = print(metrs/1000) """
+
+""" osnov = int(input("Enter the number: "))
+visota = int(input("Enter the number: "))
+s = print(osnov*visota/2) """
+
+number = int(input("Enter the number: "))
+num1 = print(number%10, number%100//10, number%1000//100, number//1000, sep = "")
